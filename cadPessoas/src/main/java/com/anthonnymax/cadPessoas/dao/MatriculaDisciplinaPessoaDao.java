1f869e4b8e7e7b1403adb9e5bd9537e5fa347d17
AnthonnyMax <anthonnyvall11@gmail.com>
2021-03-01 17:13:06 -0300

    Atualização de MatriculaDisciplinaPessoa rest / dao

---
+++ cadPessoas/src/main/java/com/anthonnymax/cadPessoas/dao/MatriculaDisciplinaPessoaDao.java
@@ -0,0 +1,9 @@
+package com.anthonnymax.cadPessoas.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anthonnymax.cadPessoas.entidade.MatriculaDisciplinaPessoa;
+
+public interface MatriculaDisciplinaPessoaDao extends JpaRepository<MatriculaDisciplinaPessoa, Long> {
+
+}
